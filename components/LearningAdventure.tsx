d4b48f3035a1601187233ce3a05e44e75bf7a210
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-10-04 16:15:35 +0100

    collabs

--- components/LearningAdventure.tsx
+++ components/LearningAdventure.tsx
@@ -1,15 +1,53 @@
+"use client"
 import React from 'react';
 import HeroSection from './Hero';
-import duo from "./Images/svg/logotype-dark.svg"
+import duo from "./Images/svg/Duolingo.svg"
+import codeCov from "./Images/svg/codecov.svg"
+import codePen from "./Images/svg/codepen.svg"
+import cloud from "./Images/svg/googleCloud.svg"
+import ibm from "./Images/svg/ibm.svg"
+import magicLeap from "./Images/svg/magicLeap.svg"
+import udemy from "./Images/svg/udemy.svg"
+import userTesting from "./Images/svg/usertesting.svg"
+import {Roboto_Condensed} from "next/font/google"
 import Image from 'next/image';
+const condensedFont = Roboto_Condensed({weight: "700", subsets: ["cyrillic", "cyrillic-ext"]})
+const images = [
+  duo, codeCov, codePen, cloud, ibm, magicLeap, udemy, userTesting
+]
+
 const LearningAdventure: React.FC = () => {
   return (
-    <main className="flex flex-col bg-opacity-0 bg-white pt-5 items-center mx-auto">
+    <main className="flex flex-col bg-opacity-0 bg-white pt-5 items-center mx-auto max-w-[70%]">
         <HeroSection />
-        <h1></h1>
-        <div className="w-full max-w-[1089px] max-md:max-w-full mx-auto flex gap-4q">
-          <Image src={duo} alt="" className="h-12" />
+        <h1 className="lg:text-4xl md:text-2xl text-xl text-neutral-600 max-w-[50%] text-center" style={{fontFamily: condensedFont.style.fontFamily}}> Over 100 Collaborations Across the World </h1>
+        <div className="flex overflow-hidden group gap-3 max-w-[90%] mt-12">
+          <div className="flex space-x-4 gap-3 animate-loop-scroll group-hover:paused items-center">
+            {
+              images.map((image, index) => (
+                <Image 
+                  key={`img1-${index}`} 
+                  src={image} 
+                  alt="" 
+                  className="h-12 w-auto max-w-none" 
+                />
+              ))
+            }
+          </div>
+          <div className="flex space-x-4 gap-3 animate-loop-scroll group-hover:paused items-center" aria-hidden="true">
+            {
+              images.map((image, index) => (
+                <Image 
+                  key={`img2-${index}`} 
+                  src={image} 
+                  alt="" 
+                  className="h-12 w-auto max-w-none" 
+                />
+              ))
+            }
+          </div>
         </div>
+
     </main>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,19 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite"
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: {
+            transform: "translateX(0%)"
+          },
+          to: {
+            transform: "translateX(-100%)"
+          }
+        }
+      }
     },
   },
   plugins: [],
