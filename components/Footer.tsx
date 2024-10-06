cc10fdad108ed10a3407e0e64ac265a28b136161
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-10-06 17:09:36 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <a href="#" className="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">E-Learning</span>
               </a>
@@ -18,16 +18,16 @@ function Footer() {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">E-Learning</a>
+                          <a href="#" className="hover:underline">E-Learning</a>
                       </li>
                       <li className="mb-4">
-                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                          <a href="#" className="hover:underline">Tailwind CSS</a>
                       </li>
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">E-Learning</a>
+                          <a href="#" className="hover:underline">E-Learning</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                          <a href="#" className="hover:underline">Tailwind CSS</a>
                       </li>
                   </ul>
               </div>
@@ -35,16 +35,16 @@ function Footer() {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                          <a href="#" className="hover:underline ">Github</a>
                       </li>
                       <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Facebook</a>
+                          <a href="#" className="hover:underline ">Facebook</a>
                       </li>
                       <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Instagram</a>
+                          <a href="#" className="hover:underline ">Instagram</a>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                          <a href="#" className="hover:underline">Discord</a>
                       </li>
                   </ul>
               </div>
@@ -63,7 +63,7 @@ function Footer() {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">E-Learning™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" className="hover:underline">E-Learning™</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
