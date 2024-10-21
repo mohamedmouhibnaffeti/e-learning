e13b5135a9cbf9984bb159e3e30b9a5fbd2e3ed5
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-10-21 20:30:31 +0100

    mentors page

---
+++ app/mentors/page.tsx
@@ -0,0 +1,99 @@
+import { Input } from '@/components/ui/input'
+import { SearchIcon } from 'lucide-react'
+import React from 'react'
+
+import mentor1 from "../../components/Images/mentors/mentor1.jpg"
+import mentor2 from "../../components/Images/mentors/mentor2.jpg"
+import mentor3 from "../../components/Images/mentors/mentor3.jpg"
+import mentor4 from "../../components/Images/mentors/mentor4.jpg"
+import MentorCard from '@/components/Cards/MentorCard'
+
+const mentors = [
+    {
+        name: "Mouhib Naffeti",
+        email: "mouhib@gmail.com",
+        jobTitle: "Full Stack Developer",
+        jobLocation: "Sousse, Tunisia",
+        image: mentor1,
+        courses: 4
+    },
+    {
+        name: "Amira Ben Ali",
+        email: "amira.benali@yahoo.com",
+        jobTitle: "Data Scientist",
+        jobLocation: "Tunis, Tunisia",
+        image: mentor2,
+        courses: 22
+    },
+    {
+        name: "Sami Bouaziz",
+        email: "sami.bouaziz@outlook.com",
+        jobTitle: "UI/UX Designer",
+        jobLocation: "Monastir, Tunisia",
+        image: mentor3,
+        courses: 15
+    },
+    {
+        name: "Yasmine Kacem",
+        email: "yasmine.kacem@gmail.com",
+        jobTitle: "Machine Learning Engineer",
+        jobLocation: "Gabes, Tunisia",
+        image: mentor4,
+        courses: 45
+    },
+    {
+        name: "Mouhib Naffeti",
+        email: "mouhib@gmail.com",
+        jobTitle: "Full Stack Developer",
+        jobLocation: "Sousse, Tunisia",
+        image: mentor1,
+        courses: 4
+    },
+    {
+        name: "Amira Ben Ali",
+        email: "amira.benali@yahoo.com",
+        jobTitle: "Data Scientist",
+        jobLocation: "Tunis, Tunisia",
+        image: mentor2,
+        courses: 22
+    },
+    {
+        name: "Sami Bouaziz",
+        email: "sami.bouaziz@outlook.com",
+        jobTitle: "UI/UX Designer",
+        jobLocation: "Monastir, Tunisia",
+        image: mentor3,
+        courses: 15
+    },
+    {
+        name: "Yasmine Kacem",
+        email: "yasmine.kacem@gmail.com",
+        jobTitle: "Machine Learning Engineer",
+        jobLocation: "Gabes, Tunisia",
+        image: mentor4,
+        courses: 45
+    },
+]
+
+function Mentors() {
+  return (
+    <div className="max-w-[1200px] flex flex-col mx-auto mt-16 xl:px-16 lg:px-12 md:px-8 px-4">
+        <h1 className="self-center text-4xl max-sm:text-xl font-bold leading-relaxed tracking-wide"> Our Best Mentors </h1>
+        <div className="relative md:max-w-[300px]">
+            <Input className="" placeholder="ex. John Doe"/>
+            <SearchIcon className="absolute right-2 top-0 translate-y-2 w-5 h-5 hover:text-violet-600 transition-all duration-150 cursor-pointer" />
+        </div>
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 w-full max-w-[1980px]">
+            {
+                mentors.map((mentor, index) => {
+                    return(
+                        <MentorCard image={mentor.image} name={mentor.name} email={mentor.email} jobTitle={mentor.jobTitle} jobLocation={mentor.jobLocation} courses={mentor.courses} />
+                    )
+                })   
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Mentors
---
+++ components/Cards/MentorCard.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { ChevronRightIcon, GraduationCap, MapPin } from 'lucide-react'
+interface MentorCardProps {
+    image: any,
+    email: string,
+    name: string,
+    jobTitle: string,
+    jobLocation: string,
+    courses: number
+}
+
+function MentorCard({image, name, email, jobTitle, jobLocation,courses}: MentorCardProps) {
+    const router = useRouter() 
+    return (
+        <div className="group flex flex-col items-center rounded-lg gap-3 md:pb-3 border-2">
+            <div className="relative overflow-hidden w-full ">
+                <Image src={image} alt='' className="object-cover w-full rounded-t-lg" />
+                <div className="absolute rounded-t-lg w-full h-full bg-black/40 hidden md:flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                    <button onClick={()=>router.push("/mentors/mentor")} className="flex items-center w-fit justify-center text-sm text-white bg-black hover:bg-black/80 transition-all duration-100 py-4 px-4 rounded-md">
+                        Visit Mentor
+                        <ChevronRightIcon className="w-4 h-4 transition duration-200" />
+                    </button>
+                </div>
+            </div>
+            <div className="w-full px-2">
+                <div className="max-w-full flex justify-between items-center">
+                    <h1 className="font-semibold text-sm text-purple-900 truncate"> {name} </h1>
+                    <p className="flex text-xs items-center gap-1"> {courses} <GraduationCap className="w-4 h-4" /> </p>
+                </div>
+                <p className="text-gray-400 text-xs ml-1 mt-2"> {jobTitle} </p>
+                <p className="text-gray-400 text-xs flex items-center"> <MapPin className="w-4 h-4" /> {jobLocation} </p>
+            </div>
+            <button onClick={()=>router.push("/mentors/mentor")} className="flex md:hidden bg-black text-white w-full py-3 active:bg-black/80 transition-all duration-100 delay-75 justify-center items-center rounded-b-lg"> <span>Visit Mentor</span> </button>
+        </div>
+    )
+}
+
+export default MentorCard
