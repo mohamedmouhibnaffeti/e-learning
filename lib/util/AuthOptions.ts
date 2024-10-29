9473d424f196e757384908b4de2cb89431f1e899
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-10-29 12:17:39 +0100

    google auth, link to database

--- lib/util/AuthOptions.ts
+++ lib/util/AuthOptions.ts
@@ -1,6 +1,25 @@
 import { SignToken } from "@/lib/util/SIgnToken"
+import { Account, Profile, Session, User } from "next-auth"
+import { AdapterUser } from "next-auth/adapters"
 import FacebookProvider from "next-auth/providers/facebook"
 import GoogleProvider from "next-auth/providers/google"
+import prisma from "./db"
+import { Roles } from "@prisma/client"
+import { JWT } from "next-auth/jwt"
+
+export interface CustomUser {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    id?: string; // Optional id field
+    provider?: string; // Custom provider field
+    role?: Roles
+}
+
+interface CustomSession extends Session {
+    user?: CustomUser;  // Use the custom user type in the session
+}
+  
 
 const AuthOptions = {
     pages:  {
@@ -15,7 +34,42 @@ const AuthOptions = {
             clientId: process.env.GOOGLE_ID as string,
             clientSecret: process.env.GOOGLE_SECRET as string,
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({user, account, profile}: {user: User | AdapterUser, account: Account | null, profile?: Profile | undefined}){
+            const FoundUser = await prisma.user.findFirst({where: {email: user?.email as string, provider: account?.provider as string}})
+            if(FoundUser){
+                return true
+            }else{
+                const newUser = await prisma.user.create({
+                data: {
+                    email: user.email as string,
+                    provider: account?.provider,
+                    name: user.name as string,
+                    image: user?.image as string
+                }
+                })
+                return true
+            }
+        },
+        async jwt({token, user, account, profile, trigger, isNewUser}: {token: JWT, user: User | AdapterUser, account: Account | null, profile?: Profile | undefined, trigger?: "signIn" | "signUp" | "update" | undefined, isNewUser?: boolean}){
+            if (account) {
+              const FoundUser = await prisma.user.findFirst({where: {email: user?.email as string, provider: account?.provider as string}, select: {role: true}})
+              const userLoggedIn = SignToken(user?.email as string)
+              token.loggedUser = userLoggedIn
+              token.provider = account.provider
+              token.role = FoundUser?.role
+            }
+            return token;
+          },
+          async session({session, token}: {session: CustomSession, token: JWT}){
+            if(token){
+              session.user = {...session.user, provider: token.provider as string, role: token.role as Roles}
+            }
+            return session
+          }
+    },
+    secret: process.env.JWT_SECRET as string,
 }
 
 export default AuthOptions
---
+++ prisma/migrations/20241029105823_usermodel/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` MODIFY `provider` VARCHAR(191) NOT NULL DEFAULT '';
