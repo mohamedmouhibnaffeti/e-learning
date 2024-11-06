c6dcdf464e2f414527caf7d62a288a64e8d6272d
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-11-06 19:40:19 +0100

    confirm course paiement

--- components/Courses/CourseDetails.tsx
+++ components/Courses/CourseDetails.tsx
@@ -108,7 +108,7 @@ function CourseDetails({course}: {course: ExtendedCourseWithLessonsAndChaptersAn
             !payed && (
                 <div className="lg:w-1/3 w-full">
                     <div className="md:mx-8 mx-2 bg-purple-400/30 flex flex-col justify-center items-center py-6 rounded-xl px-6">
-                        <h4 className="lg:text-xl md:text-lg font-semibold text-purple-600"> {course.price}TND </h4>
+                        <h4 className="lg:text-xl md:text-lg font-semibold text-purple-600"> {course.price} TND </h4>
                         
                         <GraySeperator classname="mt-5" />
                     
@@ -127,7 +127,7 @@ function CourseDetails({course}: {course: ExtendedCourseWithLessonsAndChaptersAn
                                 <Clock10Icon className="text-purple-600 h-4 w-4 -translate-y-px" />
                                 <span className="text-gray-600"> Duration </span>
                             </div>
-                            <span className="text-gray-600/70"> 1hr 15 mins </span>
+                            <span className="text-gray-600/70"> {totalDuration} </span>
                         </div>
                         
                         <GraySeperator classname="mt-5" />
@@ -137,7 +137,7 @@ function CourseDetails({course}: {course: ExtendedCourseWithLessonsAndChaptersAn
                                 <LibraryIcon className="text-purple-600 h-4 w-4 -translate-y-px" />
                                 <span className="text-gray-600"> Lessons </span>
                             </div>
-                            <span className="text-gray-600/70"> 35 </span>
+                            <span className="text-gray-600/70"> {lessonsNumber} </span>
                         </div>
                         
                         <GraySeperator classname="mt-5" />
@@ -147,7 +147,7 @@ function CourseDetails({course}: {course: ExtendedCourseWithLessonsAndChaptersAn
                                 <ChartNoAxesColumnIncreasingIcon className="text-purple-600 h-4 w-4 -translate-y-px" />
                                 <span className="text-gray-600"> Level </span>
                             </div>
-                            <span className="text-gray-600/70"> Advanced </span>
+                            <span className="text-gray-600/70"> {course.difficulty} </span>
                         </div>
                         
                         <GraySeperator classname="mt-5" />
@@ -157,14 +157,14 @@ function CourseDetails({course}: {course: ExtendedCourseWithLessonsAndChaptersAn
                                 <EarthIcon className="text-purple-600 h-4 w-4 -translate-y-px" />
                                 <span className="text-gray-600"> Language </span>
                             </div>
-                            <span className="text-gray-600/70"> English </span>
+                            <span className="text-gray-600/70"> {course.language} </span>
                         </div>
                         
                         <GraySeperator classname="mt-5" />
 
-                        <button onClick={()=>setPayed(true)} className="py-3 bg-purple-500 text-white hover:bg-purple-500/90 active:bg-purple-600 px-4 w-fit max-sm:w-full mt-7 rounded-lg transition-all duration-150">
+                        <a href="/confirm-paiement" className="py-3 bg-purple-500 text-white hover:bg-purple-500/90 active:bg-purple-600 px-4 w-fit max-sm:w-full mt-7 rounded-lg transition-all duration-150">
                             Enroll Now
-                        </button>
+                        </a>
 
                     </div>
                 </div>
